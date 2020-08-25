8a07a5a1e2391890aff4debf61cfdf4a1d89b120
bmalkmus <bmalkmus23@gmail.com>
2020-08-25 14:58:36 -0700

    first commit

---
+++ src/java_basics/Variables.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 34;
+
+        long bigNumber = 10000000000000L;
+        short smallNumber = -32767;
+        byte reallySmallNumber = 127;
+        double decimalVariable = 394.007;
+        char letter = 'G';
+
+        System.out.println(x);
+    }
+}
